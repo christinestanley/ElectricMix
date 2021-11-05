be0fe82ade5155c753471732eef7f35ad634d2d3
Christine Stanley <chris@earlswoodmarketing.com>
2021-11-05 17:19:27 +0000

    initial commit

---
+++ ElectricMix/ElectricMixApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElectricMixApp.swift
+//  ElectricMix
+//
+//  Created by Chris on 05/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct ElectricMixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
